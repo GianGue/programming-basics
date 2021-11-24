c3b3628d8087b82748fc6a5573feacfc29642483
GianGue <93381003+GianGue@users.noreply.github.com>
2021-11-24 16:38:51 +0100

    solution 006

---
+++ projects/m1/006-sum-of-digits-in-a-integer/solutions/GianlucaGuerra/solution_sum_of_digits_in_a_integer.py
@@ -0,0 +1,3 @@
+#request to insert a number of 4 digit
+value=str(input('Please insert a number of 4 digits: '))
+print('Your result is: ',value[0],'+',value[1],'+',value[2],'+',value[3],'=', str(int(value[0])+int(value[1])+int(value[2])+int(value[3])))
